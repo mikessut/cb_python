9c28ae2c8645f59227fb1d4444a0ad70365468b2
mikessut <mikesutton@gmail.com>
2018-08-05 10:41:13 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='cb_python',
+      version='0.1',
+      description='Clearbox Python Module',
+      author='Mike Sutton',
+      packages=['cb_python'],
+     )
