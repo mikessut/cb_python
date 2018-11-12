608336e13d228ace095de6c79a6e631c127f4244
mikessut <mikesutton@gmail.com>
2018-11-11 22:24:51 -0700

    mostly done and tested

---
+++ clearbox/boolexpr.py
@@ -0,0 +1,103 @@
+
+
+class Expression:
+
+  __none_result__ = False
+
+  def __init__(self, a, b, op):
+    self.a = a
+    self.b = b
+    self.op = op
+
+  def __and__(self, other):
+    return Expression(self, other, '__and__')
+
+  def _eval(self, class_parameter_list, class_parameter_result, *objs):
+    if isinstance(self.a, ClassParameter):
+        a = self.a.eval(*objs)
+        class_parameter_list.append(self.a)
+        class_parameter_result.append(a)
+    elif isinstance(self.a, Expression):
+        a = self.a._eval(class_parameter_list, class_parameter_result, *objs)
+    else:
+        a = self.a
+
+    if isinstance(self.b, ClassParameter):
+        b = self.b.eval(*objs)
+        class_parameter_list.append(self.b)
+        class_parameter_result.append(b)
+    elif isinstance(self.b, Expression):
+        b = self.b._eval(class_parameter_list, class_parameter_result, *objs)
+    else:
+        b = self.b
+
+    if (a is None) or (b is None):
+        return None  # actual expression return will be determined by BoolExprEnabledClass.__none_result__
+    return getattr(a, self.op)(b)
+
+  def eval(self, *objs):
+    class_parameter_list = []
+    class_parameter_result = []
+    result = self._eval(class_parameter_list, class_parameter_result, *objs)
+    #print(class_parameter_list, class_parameter_result)
+    if not all([x._cls.__none_result__ == class_parameter_list[0]._cls.__none_result__ for x in class_parameter_list]):
+      raise BoolExprNoneResultMismatchException("All BoolExprEnabledClass must have the same __none_result__")
+
+    if any([x is None for x in class_parameter_result]):
+        return class_parameter_list[0].__none_result__
+
+    return result
+
+
+class ClassParameter:
+
+  def __init__(self, cls, name, op=None):
+    self._cls = cls
+    self._name = name
+    self._op = op
+
+  def __lt__(self, other):
+    return Expression(self, other, '__lt__')
+
+  def __gt__(self, other):
+    return Expression(self, other, '__gt__')
+
+  def __eq__(self, other):
+    return Expression(self, other, '__eq__')
+
+  def eval(self, *objs):
+    #print(type(objs))
+    #print([type(x) for x in objs])
+    #print(self._cls)
+    #print(isinstance(objs[0], self._cls))
+    #print(self._cls.__proxy_class__)
+    obj = next(x for x in objs
+        if (isinstance(x, self._cls) or
+        (('__proxy_class__' in self._cls.__dict__.keys()) and
+        isinstance(x, self._cls.__proxy_class__))))
+    if self._op is not None:
+      return self._op(getattr(obj, self._name))
+    else:
+      return getattr(obj, self._name)
+
+  def __getattr__(self, func):
+    """Run function on parameter.
+
+    E.g. ThingA.a.len will run len(ThingA().a) when evaluated
+    """
+    return ClassParameter(self._cls, self._name, eval(func))
+  #def len(self):
+  #  return ClassParameter(self._cls, self._name, len)
+
+class BoolExprClass(type):
+
+  def __getattr__(self, attr):
+    #print("getattr", self)
+    return ClassParameter(self, attr)
+
+class BoolExprEnabledClass(metaclass=BoolExprClass):
+  __proxy_class__ = None
+  __none_result__ = None
+
+class BoolExprNoneResultMismatchException(Exception):
+    pass
---
+++ clearbox/tests/test_boolexpr.py
@@ -0,0 +1,130 @@
+import unittest
+from clearbox.boolexpr import *
+
+class TestBoolExpr(unittest.TestCase):
+
+  def test_classparameter(self):
+
+    class Thing:
+      a = 123
+
+    t = Thing()
+
+    cp = ClassParameter(Thing, 'a')
+
+    self.assertTrue(cp.eval(t), 123)
+
+  def test_expr(self):
+
+    class ThingA(BoolExprEnabledClass):
+      pass
+
+    class ThingB(BoolExprEnabledClass):
+      pass
+
+    a = ThingA()
+    a.a = 123
+    b = ThingB()
+    b.b = 456
+    b.mylist = [1,2,3,4]
+
+    e = ThingA.a < ThingB.b
+    #import pdb; pdb.set_trace()
+    self.assertTrue(e.eval(a, b))
+
+    e = 0 < ThingA.a
+    self.assertTrue(e.eval(a,b))
+
+    e2 = ThingB.mylist.len > 2
+    self.assertTrue(e2.eval(a,b))
+
+    e2 = 2 < ThingB.mylist.len
+    self.assertTrue(e2.eval(a,b))
+
+    e3 = (ThingA.a == 123)
+    self.assertTrue(e3.eval(a,b))
+
+    e3 = (123 == ThingA.a)
+    self.assertTrue(e3.eval(a,b))
+
+    e4 = (ThingB.mylist.len > 2) & (ThingA.a == 123)
+    self.assertTrue(e4.eval(a,b))
+
+    e = (ThingB.mylist.len > 2) & (ThingA.a == 122)
+    self.assertFalse(e.eval(a,b))
+
+    e = ThingB.mylist.max == 4
+    self.assertTrue(e.eval(b))
+
+  def test_expr_proxy(self):
+    class ThingA:
+      pass
+
+    class ThingAProxy(BoolExprEnabledClass):
+      __proxy_class__ = ThingA
+
+    class ThingB:
+      pass
+
+    class ThingBProxy(BoolExprEnabledClass):
+      __proxy_class__ = ThingB
+
+    a = ThingA()
+    a.a = 123
+    b = ThingB()
+    b.b = 456
+    b.mylist = [1,2,3,4]
+
+    e = ThingAProxy.a < ThingBProxy.b
+    #import pdb; pdb.set_trace()
+    self.assertTrue(e.eval(a, b))
+
+    e = 0 < ThingAProxy.a
+    self.assertTrue(e.eval(a,b))
+
+    e2 = ThingBProxy.mylist.len > 2
+    self.assertTrue(e2.eval(a,b))
+
+    e2 = 2 < ThingBProxy.mylist.len
+    self.assertTrue(e2.eval(a,b))
+
+    e3 = (ThingAProxy.a == 123)
+    self.assertTrue(e3.eval(a,b))
+
+    e3 = (123 == ThingAProxy.a)
+    self.assertTrue(e3.eval(a,b))
+
+    e4 = (ThingBProxy.mylist.len > 2) & (ThingAProxy.a == 123)
+    self.assertTrue(e4.eval(a,b))
+
+    e = (ThingBProxy.mylist.len > 2) & (ThingAProxy.a == 122)
+    self.assertFalse(e.eval(a,b))
+
+    e = ThingBProxy.mylist.max == 4
+    self.assertTrue(e.eval(b))
+
+  def test_none_result_mismatch(self):
+    class ThingA(BoolExprEnabledClass):
+      __none_result__ = False
+
+    class ThingB(BoolExprEnabledClass):
+       __none_result__ = True
+
+    e = (ThingA.foo > 3) & (ThingB.bar == 3)
+    a = ThingA()
+    a.foo = None
+    b = ThingB()
+    b.bar = None
+    #print(e.eval(a, b))
+    self.assertRaises(BoolExprNoneResultMismatchException, e.eval, *(a, b))
+
+def test_none_result(self):
+
+    class ThingA(BoolExprEnabledClass):
+        __none_result__ = False
+
+    a = ThingA()
+    a.weird = None
+    e = ThingA.weird == 4
+    #import pdb; pdb.set_trace()
+    self.assertFalse(e.eval(a))
