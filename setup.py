0fee077213e05f2799b7c23ef47783379d290ea9
mikessut <mikesutton@gmail.com>
2018-11-12 14:37:44 -0700

    new version for boolexpr

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='clearbox',
-      version='0.1',
+      version='0.2',
       description='Clearbox Python Module',
       author='Mike Sutton',
       packages=['clearbox'],
