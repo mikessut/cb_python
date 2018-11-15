8f32519ff3bcb5c10127a7d3d9a5bd06673b0763
mikessut <mikesutton@gmail.com>
2018-11-15 06:16:24 -0700

    roll version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='clearbox',
-      version='0.2',
+      version='0.21',
       description='Clearbox Python Module',
       author='Mike Sutton',
       packages=['clearbox'],
