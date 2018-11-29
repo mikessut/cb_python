b3d214c0be23b8731a29cced11730ef69e834721
mikessut <mikesutton@gmail.com>
2018-11-29 02:29:05 -0700

    added expr.abs() functionality setup ver

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='clearbox',
-      version='0.23',
+      version='0.24',
       description='Clearbox Python Module',
       author='Mike Sutton',
       packages=['clearbox'],
